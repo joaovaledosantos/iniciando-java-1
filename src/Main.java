0a64f6935f47ad6eaca698411c9d81e22d9b5f4b
João V. Santos <joaovaledosantos@gmail.com>
2024-02-21 06:25:32 -0300

    [Aula-13] Atalhos e formatação

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        //Dados do clinte
         byte minhaIdade = 18;
-        String firstName = "João " + "Santos";
+        String firstName = "João " + "\tSantos";
+        //Imprimir na tela dos dados acima
 
         System.out.println(minhaIdade);
+        System.out.println("Hi");
         System.out.println(firstName.toLowerCase());
     }
 }
