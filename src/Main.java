3eebbd8fbe16b57681a1cd9ca609803eae19c4fd
João V. Santos <joaovaledosantos@gmail.com>
2024-02-21 06:41:19 -0300

    [Aula-14] Adicionando o Input

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //Dados do clinte
-        byte minhaIdade = 18;
-        String firstName = "João " + "\tSantos";
-        //Imprimir na tela dos dados acima
-
-        System.out.println(minhaIdade);
-        System.out.println("Hi");
-        System.out.println(firstName.toLowerCase());
+        Scanner idadeScanner = new Scanner(System.in);
+        System.out.println("Digite a sua idade: ");
+        byte idadeCliente = idadeScanner.nextByte();
+        System.out.println(idadeCliente);
     }
 }
 
