6d54c15c971c7894d3a911df92d74350ecfbae01
João V. Santos <joaovaledosantos@gmail.com>
2024-02-21 03:45:44 -0300

    [Aula-10] Tipos primitivos de variáveis

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int minhaIdade = 18;
-        int salarioMensal = 50000;
+        byte minhaIdade = 18;
+        int salarioMensal = 50_000;
+        long idadeMilisegundos = 788400000000L;
+        double altura = 1.80;
+        char initial = 'J';
+        boolean eMaiorDeIdade = true;
         System.out.println(minhaIdade);
         System.out.println(salarioMensal);
     }
 }
+
