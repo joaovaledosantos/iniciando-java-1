f7860258ddd49a221d457e65a01b8db427f6d518
João V. Santos <joaovaledosantos@gmail.com>
2024-02-21 03:59:25 -0300

    [Aula-12]  Tipos de referência de variáveis

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         byte minhaIdade = 18;
-        int salarioMensal = 50_000;
-        long idadeMilisegundos = 788400000000L;
-        double altura = 1.80;
-        char initial = 'J';
-        boolean eMaiorDeIdade = true;
+        String firstName = "João " + "Santos";
+
         System.out.println(minhaIdade);
-        System.out.println(salarioMensal);
+        System.out.println(firstName.toLowerCase());
     }
 }
 
